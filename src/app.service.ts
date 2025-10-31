9c255865778d85f3bd287e4820e93935e589f7c9
Phanesan <yahir.romo@hotmail.com>
2025-10-30 19:21:10 -0700

    ✨ feat(health): add health check endpoint
    - implement a /health endpoint to monitor application status
    - the endpoint returns a JSON object with a status of 'ok'

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AppService {}
+export class AppService {
+  getHealth(): { status: string; message: string } {
+    return { status: 'ok', message: 'Backend is running' };
+  }
+}
